0429a8d9c2c2321ce2430e448d2442d854e3f59e
endkarn <end_karn@hotmail.com>
2019-08-26 15:25:38 +0700

    [Added]: เพิ่ม กรณีทดสอบของกรณีของ buzz , fizzbuzz และเลขที่นอกเหอยเงื่อนไข

--- fizzbuzz/service/service.go
+++ fizzbuzz/service/service.go
@@ -1,5 +1,7 @@
 package service
 
+import "strconv"
+
 type FizzBuzzService interface {
 	GetFizzBuzz(number int) string
 }
@@ -9,5 +11,14 @@ type FizzBuzz struct {
 }
 
 func (fizzbuzz FizzBuzz) GetFizzBuzz(number int) string {
-	return "fizz"
+	if number%15 == 0 {
+		return "fizzbuzz"
+	}
+	if number%5 == 0 {
+		return "buzz"
+	}
+	if number%3 == 0 {
+		return "fizz"
+	}
+	return strconv.Itoa(number)
 }
--- fizzbuzz/service/service_test.go
+++ fizzbuzz/service/service_test.go
@@ -15,3 +15,33 @@ func Test_GetFizzBuzzNumber_Input_3_Should_Be_Fizz(t *testing.T){
 
 	assert.Equal(t, expected, string(actual))
 }
+
+func Test_GetFizzBuzzNumber_Input_5_Should_Be_Fizz(t *testing.T){
+	expected := "buzz"
+	number := 5
+	fizzBuzzService := service.FizzBuzz{}
+
+	actual := fizzBuzzService.GetFizzBuzz(number)
+
+	assert.Equal(t, expected, string(actual))
+}
+
+func Test_GetFizzBuzzNumber_Input_15_Should_Be_FizzBuzz(t *testing.T){
+	expected := "fizzbuzz"
+	number := 15
+	fizzBuzzService := service.FizzBuzz{}
+
+	actual := fizzBuzzService.GetFizzBuzz(number)
+
+	assert.Equal(t, expected, string(actual))
+}
+
+func Test_GetFizzBuzzNumber_Input_8_Should_Be_8(t *testing.T){
+	expected := "8"
+	number := 8
+	fizzBuzzService := service.FizzBuzz{}
+
+	actual := fizzBuzzService.GetFizzBuzz(number)
+
+	assert.Equal(t, expected, string(actual))
+}
